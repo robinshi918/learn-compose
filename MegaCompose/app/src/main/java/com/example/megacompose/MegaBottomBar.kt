ece15aff10e5cd9c83a24dcf9f99e60e453806de
Robin Shi <robinshi918@hotmail.com>
2021-12-04 09:38:47 +1300

    learning codes

--- MegaCompose/app/src/main/java/com/example/megacompose/MegaBottomBar.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/MegaBottomBar.kt
@@ -1,19 +1,25 @@
 package com.example.megacompose
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.megacompose.ui.theme.MegaComposeTheme
 
 
 @Composable
@@ -72,5 +78,62 @@ fun TabItem(@DrawableRes iconId: Int, modifier: Modifier = Modifier) {
         contentAlignment = Alignment.Center
     ) {
         Icon(painter = painterResource(iconId), contentDescription = "")
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        }
+    }
+}
+
+////////////////////////////////////////
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+
+    Row(modifier = modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { println() }
+        .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)  //?????
+        ) {
+            // image goes here
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha.provides(ContentAlpha.medium)) {  // ?????
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
+
+}
+
+
+@Preview
+@Composable
+fun PhotographCardPreview() {
+    MegaComposeTheme {
+        PhotographerCard()
+    }
+}
+
+@Preview
+@Composable
+fun playWithButton() {
+    Button(onClick = { /*TODO*/ }) {
+        Column {
+            Text(text = "line 1")
+            Text("line 2")
+
+        }
     }
 }
