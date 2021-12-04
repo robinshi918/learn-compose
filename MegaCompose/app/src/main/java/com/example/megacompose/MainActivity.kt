7f6bbdda9b28002bb5774eebb7b14d4fe1a948fb
Robin Shi <robinshi918@hotmail.com>
2021-12-05 07:11:21 +1300

    add more learning code

--- MegaCompose/app/src/main/java/com/example/megacompose/MainActivity.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/MainActivity.kt
@@ -3,11 +3,23 @@ package com.example.megacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.megacompose.ui.theme.MegaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MegaComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.secondary) {
                     MegaBottomBar(selected = 0) {
 
                     }
@@ -38,3 +50,61 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+@Preview
+@Composable
+fun LayoutScaffold() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Hello Title")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Face, contentDescription = "")
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Delete, contentDescription = "")
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Face, contentDescription = "")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(text = "Hi There!")
+            Text(text = "Thanks for going through the Layouts lesson")
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun SimpleList(modifier: Modifier = Modifier) {
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(index = it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
