800bf4315edca59fec98a07ee9efab1b3a488398
Robin Shi <robinshi918@hotmail.com>
2021-12-25 22:41:30 +1300

    show correct icons in bottom bar

--- MegaCompose/app/src/main/java/com/example/megacompose/ui/BottomBarItem.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/ui/BottomBarItem.kt
@@ -3,17 +3,20 @@ package com.example.megacompose.ui
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.megacompose.R
 
 sealed class BottomBarScreen(
     val route: String,
     val title: String,
     val icon: ImageVector,
+    val selectedIconRes: Int,
     val unselectedIcon: ImageVector
 ) {
     object Home : BottomBarScreen(
         route = "home",
         title = "Home",
         icon = Icons.Default.Home,
+        selectedIconRes = R.drawable.ic_homepage,
         unselectedIcon = Icons.Default.NoAccounts
     )
 
@@ -21,6 +24,7 @@ sealed class BottomBarScreen(
         route = "clouddrive",
         title = "Cloud Drive",
         icon = Icons.Default.FilePresent,
+        selectedIconRes = R.drawable.ic_files_home,
         unselectedIcon = Icons.Default.NoAccounts
     )
 
@@ -28,6 +32,7 @@ sealed class BottomBarScreen(
         route = "photo",
         title = "Photo",
         icon = Icons.Default.Image,
+        selectedIconRes = R.drawable.ic_camera_uploads,
         unselectedIcon = Icons.Default.NoAccounts
     )
 
@@ -35,6 +40,7 @@ sealed class BottomBarScreen(
         route = "chat",
         title = "Chat",
         icon = Icons.Default.Chat,
+        selectedIconRes = R.drawable.ic_chat,
         unselectedIcon = Icons.Default.NoAccounts
     )
 
@@ -42,6 +48,7 @@ sealed class BottomBarScreen(
         route = "transfer",
         title = "Transfer",
         icon = Icons.Default.Transform,
+        selectedIconRes = R.drawable.ic_shared,
         unselectedIcon = Icons.Default.NoAccounts
     )
 
