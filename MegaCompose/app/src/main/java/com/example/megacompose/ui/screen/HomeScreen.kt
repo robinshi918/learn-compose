05649f699df7c7b115ca6d93ae79c7eaea67cb2e
Robin Shi <robinshi918@hotmail.com>
2021-12-28 22:55:04 +1300

    add viewpager and tabs in Home page

--- MegaCompose/app/src/main/java/com/example/megacompose/ui/screen/HomeScreen.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/ui/screen/HomeScreen.kt
@@ -1,16 +1,18 @@
 package com.example.megacompose.ui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.megacompose.ui.theme.MegaComposeTheme
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen() {
@@ -20,17 +22,72 @@ fun HomeScreen() {
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "HOME",
-            fontSize = 40.sp
-        )
+//        Text(
+//            text = "HOME",
+//            fontSize = 40.sp
+//        )
+        HomeTabs()
     }
 }
 
-@Preview
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun previewHomeScreen() {
-    MegaComposeTheme {
-        HomeScreen()
+fun HomeTabs() {
+    val tabData = listOf(
+        "Recent",
+        "Offline"
+    )
+    val pagerState = rememberPagerState(
+        pageCount = tabData.size,
+        initialOffscreenLimit = 2,
+        initialPage = 0
+    )
+
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        val indicator = @Composable { _: List<TabPosition> ->
+            Surface(modifier = Modifier
+                .height(2.dp)
+                .background(color = Color.Black)) {}
+        }
+
+        TabRow(
+            selectedTabIndex = tabIndex,
+            backgroundColor = Color.Transparent,
+            contentColor = Color.Red
+        ) {
+            tabData.forEachIndexed { index, label ->
+                Tab(selected = index == tabIndex,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    }, text = {
+                        Text(
+                            text = label,
+                            color = if (index == tabIndex) Color.Red else Color.Gray
+                        )
+                    }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { index ->
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = tabData[index])
+            }
+        }
     }
+
+
 }
+
