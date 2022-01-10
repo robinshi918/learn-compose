bf6160e1c07ce541cd649acc8be9a6bc7b49c97b
Robin Shi <rsh@mega.co.nz>
2022-01-10 15:44:21 +1300

    refactor: move home title bar to HomeScreen

--- MegaCompose/app/src/main/java/com/example/megacompose/MainScreen.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/MainScreen.kt
@@ -1,29 +1,21 @@
 package com.example.megacompose
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.megacompose.ui.screen.NavigationView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(
@@ -56,61 +48,6 @@ fun shouldShowBottomNavBar(navController: NavHostController): Boolean {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val route = navBackStackEntry?.destination?.route
     return !(route != null && route == MegaScreen.Login.route)
-}
-
-@Composable
-fun HomeTitleBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
-    Surface(
-        elevation = 4.dp,
-        modifier = Modifier
-            .height(60.dp)
-            .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 4.dp),
-        shape = RoundedCornerShape(4.dp)
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            val textState = remember { mutableStateOf(TextFieldValue()) }
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = textState.value,
-                onValueChange = { textState.value = it },
-                colors = TextFieldDefaults.textFieldColors(
-                    backgroundColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent
-                ),
-                singleLine = true,
-                placeholder = {
-                    Text(
-                        text = "Search in MEGA",
-                        fontSize = 15.sp,
-                    )
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Menu,
-                        contentDescription = "settings button",
-                        modifier = Modifier.clickable {
-                            println("setting icon clicked")
-                            scope.launch { scaffoldState.drawerState.open() }
-                        }
-                    )
-                },
-                trailingIcon = {
-                    Surface(
-                        modifier = Modifier
-                            .size(25.dp),
-                        shape = CircleShape,
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 1.0f),
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.avatar),
-                            contentDescription = "avatar"
-                        )
-                    }
-                }
-            )
-        }
-    }
 }
 
 @Composable
--- MegaCompose/app/src/main/java/com/example/megacompose/home/HomeScreen.kt
+++ MegaCompose/app/src/main/java/com/example/megacompose/home/HomeScreen.kt
@@ -1,18 +1,28 @@
 package com.example.megacompose.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.megacompose.HomeTitleBar
+import androidx.compose.ui.unit.sp
+import com.example.megacompose.R
+import com.example.megacompose.ui.theme.MegaComposeTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
@@ -97,4 +107,65 @@ fun HomeTabs() {
 
 
 }
+@Preview
+@Composable
+fun PreviewbAR() {
+    MegaComposeTheme() {
+        HomeTitleBar(scaffoldState =afds , scope = asfasd)
+    }
+}
 
+@Composable
+fun HomeTitleBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
+    Surface(
+        elevation = 4.dp,
+        modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 4.dp),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            val textState = remember { mutableStateOf(TextFieldValue()) }
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = textState.value,
+                onValueChange = { textState.value = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = "Search in MEGA",
+                        fontSize = 15.sp,
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "settings button",
+                        modifier = Modifier.clickable {
+                            println("setting icon clicked")
+                            scope.launch { scaffoldState.drawerState.open() }
+                        }
+                    )
+                },
+                trailingIcon = {
+                    Surface(
+                        modifier = Modifier
+                            .size(25.dp),
+                        shape = CircleShape,
+                        color = MaterialTheme.colors.onSurface.copy(alpha = 1.0f),
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.avatar),
+                            contentDescription = "avatar"
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
